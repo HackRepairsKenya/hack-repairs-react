88c2dc79954747302b1b34d9401981c61ac6be63
GRACE1-GG <gracewema607@gmail.com>
2024-09-29 09:36:44 -0700

    return policy

---
+++ src/pages/ReturnPolicy.tsx
@@ -0,0 +1,73 @@
+const ReturnPolicy = () => {
+    return(
+        <div className="return-policy">
+            <h1>Return policy</h1>
+<p>At Hack Repairs, we strive to ensure your satisfaction with every purchase of our phone & repair,
+     laptop & repair and other services. We understand that circumstances may arise where a return or exchange becomes necessary.
+     Please read our return policy carefully to understand your rights and obligations:</p>
+
+     <h2>1. Returns Eligibility:</h2>
+     <ul class="list-decimal">
+  <li>We accept returns within 30 days from the date of purchase. To be eligible for a return, the item must be unused, 
+    in its original packaging, and in the same condition as received.</li>
+  
+</ul>
+<h2>2. Non-Returnable Items:</h2>
+<ul class="list-decimal">
+  <li>Certain items are not eligible for return or refund due to hygiene reasons or specific manufacturer restrictions. These include opened broken phones, broken laptops and other items
+     . Please check with our customer service for details.</li>
+  
+</ul>
+<h2>3. Return Process:</h2>
+<ul class="list-decimal">
+  <li>Contact our customer service team to initiate the return process. You can reach us by +254 741 699 821. Provide your order number, the item(s) you wish to return, and the reason for the return.
+     Our team will guide you through the next steps, including the return shipping process if applicable.</li>
+  
+</ul>
+<h2>4. Refund or Exchange:</h2>
+<ul class="list-decimal">
+  <li>Upon receipt and inspection of the returned item(s), we will notify you of the approval or rejection of your refund or exchange. If approved, refunds will be processed within 3 to the original method of payment. 
+    Exchanges will be processed promptly, subject to availability of the replacement item.</li>
+  
+</ul>
+<h2>5. Restocking fee:</h2>
+<ul class="list-decimal">
+  <li>A restocking fee may apply for certain returns, especially for large or specialized items.
+     This fee will be deducted from your refund.</li>
+  
+</ul>
+<h2>6. Damaged or Defective Items:</h2>
+<ul class="list-decimal">
+  <li>
+  In case of receiving a damaged or defective item, please contact us immediately. 
+  We will arrange for a replacement or repair as per manufacturer's warranty guidelines</li>
+  
+</ul>
+<h2>7. Return shipping:</h2>
+<ul class="list-decimal">
+  <li>Customers are responsible for return shipping costs unless the return is due to our error or a defective product.</li>
+  
+</ul>
+<h2>8. Cancellation Policy:</h2>
+<ul class="list-decimal">
+  <li>Orders can be cancelled before they are shipped. Once shipped, the standard return policy applies</li>
+  
+</ul>
+<h2>9. Contact Us:</h2>
+<ul class="list-decimal">
+  <li>For any questions regarding our return policy or to initiate a return, 
+    please contact Hack Repairs at info@hackrepairs.co.ke.</li>
+  
+</ul>
+<h2>10. Policy Changes:</h2>
+<ul class="list-decimal">
+  <li>Hack Repairs reserves the right to modify this return policy at any time. 
+    Changes will be effective immediately upon posting on our website. By making a purchase from Hack Repairs, 
+    you agree to abide by this return policy. We value your business and are committed to providing a smooth and fair return process. Thank you for choosing Hack Repairs for ou services
+     </li>
+  </ul>
+        </div>
+    );
+}
+
+export default ReturnPolicy;
